91a9875625ad3aeab04dd009c0659ddd69b0407d
Maciej Janta-Lipinski <maciekjl1993@gmail.com>
2023-04-19 19:35:55 +0200

    finished

--- src/js/lib/maps.js
+++ src/js/lib/maps.js
@@ -1,3 +1,21 @@
+import L from 'leaflet';
+
 export default () => {
-    console.log('Hello maps!');
+    const mapEl = document.getElementById('map');
+    if (mapEl) {
+        const map = L.map(mapEl,{
+            center: [54.4524299,17.0418873],
+            zoom: 14,
+            gestureHandling: true
+        });
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 25,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        const marker = L.marker([54.4524299,17.0418873]).addTo(map);
+
+        marker.bindPopup("<b>Tutaj teraz jest Akademia</b><br>A od czerwca będzie Uniwersytet.");
+
+    }
 }
+
